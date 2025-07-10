8a5cc990ee832e9431b0b6b4c2371fa48ffb79e1
Sunlight419 <mintialol72@gmail.com>
2025-07-10 22:04:23 +0900

    全消し

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-mod bitboard;
-
 fn main() {
-    let mut board = bitboard::Board::default();
-    board.set_xy(0, 1, 1);
-    board.set_xy(0, 2, 3);
-    board.set_xy(1, 2, 4);
-    println!("{}", board);
+
 }
